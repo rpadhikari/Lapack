7781abe734d113555b0fd6c7668250c8f1bd6e57
Rajendra Adhikari <rpadhikari@gmail.com>
2021-10-27 07:20:16 +0545

    Create dsyev.f90

---
+++ EigSym/dsyev.f90
@@ -0,0 +1,47 @@
+! This program computes all eigenvalues and eigenvectors of 
+! a real symmetric matrix A(4,4) using DSYEV subroutine.
+! 5.0 4.0 1.0 1.0
+! 4.0 5.0 1.0 1.0
+! 1.0 1.0 4.0 2.0
+! 1.0 1.0 2.0 4.0
+
+! With eigenvalues as below:
+! 1.0 2.0 5.0 10.0
+! and eigenvectors are:
+! -------------  
+ 
+program main
+  implicit none
+  integer(4), parameter :: N = 4
+  integer(4), parameter :: LDA = 4
+  integer(4) :: INFO
+  integer(4) :: WORK(1, 3*N - 1), LWORK(1, 3*N - 1)
+  real(8) :: A(LDA, N), W(N)
+  integer(4) :: i, j
+  open(1,file='amat.dat', action='read', status='old')
+ 
+  ! reading matrix A from file 'amat.dat'
+  do i=1,N
+    read(1,*) (A(i,j), j=1,N)
+  end do
+  close(1)
+
+  call DSYEV('V', 'U', N, A, LDA, W, WORK, LWORK, INFO)
+
+!  if(INFO .gt. 0) then
+!    write(*,*)' Failed to compute eigenvalues or eigenvectors'
+!  else
+  
+!  print eigenvalues  
+    do i=1, N
+      write(*, 100), w(i)
+    end do
+    100 format(f10.5)
+
+! print eigenvectors
+    do i=1, N
+       write(*,101) (A(i, j), j=1,N)
+    end do
+    101 format(4f10.5)
+!  end if
+end program main
